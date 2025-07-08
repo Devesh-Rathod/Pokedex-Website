15ad101318da5ef83d930310975e2489ce9f4743
devesh <deveshrathod15@gmail.com>
2025-07-09 02:28:29 +0530

    Almost fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import ghPages from 'vite-plugin-gh-pages'
 
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: '/YOUR_REPO_NAME/', // Replace with your repo name
+  base: '/Pokedex-Website/', // <-- use your repo name here
 })
