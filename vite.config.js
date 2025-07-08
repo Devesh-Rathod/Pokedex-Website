2d957b81ae946dbf91d11c9cdaf4a5d4fb1da096
devesh <deveshrathod15@gmail.com>
2025-07-09 02:54:28 +0530

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages'
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/Pokedex-Website/', // <-- use your repo name here
+  plugins: [react()],
+  base: '/Pokedex-Website/', // keep your repo name here
 })
